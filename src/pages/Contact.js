3298ef228a2403122efa088467a45463e3db1627
Luke Thomis <luke.thomis@gmail.com>
2022-04-13 20:54:01 -0400

    created contact page and added content.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Layout from '../layout'
 
+import contactImg from '../images/bgImage.png';
+
 function Contact() {
   return (
     <Layout>
-    <div>Contact</div>
+      <div className='contact-wrapper'>
+        <div className='left-side-wrapper'>
+          <img src={contactImg} alt='sidebar image'/>
+        </div>
+        <div className='right-side-wrapper'>
+          <div className='contact-wrapper'>
+            <div className='contact-header'>Contact Me</div>
+            <div className='contact-phone'>(270) 508-0383</div>
+            <div className='contact-email'>matt@email.com</div>
+            <div className='contact-address'>Paducah, KY</div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
